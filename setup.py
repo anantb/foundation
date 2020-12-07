51a18014835750ec3002882c5037842a30573705
Aaron Vontell <vontell@mit.edu>
2020-12-07 09:53:54 -0500

    Use new protobuf version

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ setup(
         'mypy==0.701',
         'mypy-extensions==0.4.3',
         'mypy-protobuf==1.9',
-        'protobuf==3.6.1',
+        'protobuf==3.12.2',
         'six==1.15.0',
         'typed-ast==1.3.5',
         'typing-extensions==3.7.4.3',
